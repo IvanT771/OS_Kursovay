a46fbd314e820356254063f3a7227e9a294c230f
IvanT771 <cnyazev.ivan@yandex.ru>
2021-12-19 16:10:15 +0300

    Обновил интерфейс у сервера №2

--- ClientOS/ClientOS/FormServer2.Designer.cs
+++ ClientOS/ClientOS/FormServer2.Designer.cs
@@ -28,52 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
             this.textBoxVirtualMemory = new System.Windows.Forms.TextBox();
             this.textBoxPhysicMemory = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(158, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(88, 24);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Server 2";
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(12, 288);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Выйти";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBoxVirtualMemory
             // 
+            this.textBoxVirtualMemory.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(51)))), ((int)(((byte)(72)))));
             this.textBoxVirtualMemory.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBoxVirtualMemory.Location = new System.Drawing.Point(208, 134);
+            this.textBoxVirtualMemory.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(211)))), ((int)(((byte)(148)))));
+            this.textBoxVirtualMemory.Location = new System.Drawing.Point(343, 51);
             this.textBoxVirtualMemory.Name = "textBoxVirtualMemory";
             this.textBoxVirtualMemory.ReadOnly = true;
-            this.textBoxVirtualMemory.Size = new System.Drawing.Size(156, 29);
+            this.textBoxVirtualMemory.Size = new System.Drawing.Size(200, 29);
             this.textBoxVirtualMemory.TabIndex = 2;
             this.textBoxVirtualMemory.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
             // textBoxPhysicMemory
             // 
+            this.textBoxPhysicMemory.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(51)))), ((int)(((byte)(72)))));
             this.textBoxPhysicMemory.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBoxPhysicMemory.Location = new System.Drawing.Point(208, 196);
+            this.textBoxPhysicMemory.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(211)))), ((int)(((byte)(148)))));
+            this.textBoxPhysicMemory.Location = new System.Drawing.Point(343, 96);
             this.textBoxPhysicMemory.Name = "textBoxPhysicMemory";
             this.textBoxPhysicMemory.ReadOnly = true;
-            this.textBoxPhysicMemory.Size = new System.Drawing.Size(156, 29);
+            this.textBoxPhysicMemory.Size = new System.Drawing.Size(200, 29);
             this.textBoxPhysicMemory.TabIndex = 3;
             this.textBoxPhysicMemory.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
@@ -81,7 +65,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(8, 139);
+            this.label2.Location = new System.Drawing.Point(16, 54);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(200, 24);
             this.label2.TabIndex = 4;
@@ -91,7 +75,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label3.Location = new System.Drawing.Point(8, 201);
+            this.label3.Location = new System.Drawing.Point(16, 99);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(193, 24);
             this.label3.TabIndex = 5;
@@ -99,42 +83,58 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(268, 277);
+            this.button2.FlatAppearance.BorderColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button2.FlatAppearance.BorderSize = 3;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button2.Location = new System.Drawing.Point(144, 251);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(147, 34);
+            this.button2.Size = new System.Drawing.Size(265, 42);
             this.button2.TabIndex = 6;
-            this.button2.Text = "Получить информацию";
+            this.button2.Text = "Обновить информацию";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.buttonGetSystemInfo);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBoxVirtualMemory);
+            this.groupBox1.Controls.Add(this.textBoxPhysicMemory);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(211)))), ((int)(((byte)(148)))));
+            this.groupBox1.Location = new System.Drawing.Point(12, 50);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(549, 179);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ресурсы сервера";
             // 
             // FormServer2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(427, 338);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(51)))), ((int)(((byte)(72)))));
+            this.ClientSize = new System.Drawing.Size(573, 319);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBoxPhysicMemory);
-            this.Controls.Add(this.textBoxVirtualMemory);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
             this.Name = "FormServer2";
-            this.Text = "Server2";
+            this.Text = "Подключен к серверу №2";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FormServer2_FormClosed);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBoxVirtualMemory;
         private System.Windows.Forms.TextBox textBoxPhysicMemory;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
