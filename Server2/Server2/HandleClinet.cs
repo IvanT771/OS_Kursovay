bea7522b12c2fea88a69133f1a7cd30b35c6e477
IvanT771 <cnyazev.ivan@yandex.ru>
2021-12-19 16:02:12 +0300

    Добавил сервер №2.

---
+++ Server2/Server2/HandleClinet.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Server2
+{
+    public class HandleClinet
+    {
+        private SystemInfo _systemInfo = new SystemInfo();
+        private TcpClient _clientSocket;
+        private string _clientId;
+
+        public void StartClient(TcpClient inClientSocket, string id)
+        {
+            _clientSocket = inClientSocket;
+            _clientId = id;
+
+            //Создаем новый поток и запускаем его для общения с клиентом
+            Thread ctThread = new Thread(ClientReqest);
+            ctThread.Start();
+        }
+
+        /// <summary>
+        /// Отправляет сообщение клиенту
+        /// </summary>
+        /// <param name="message"></param>
+        private void ShowMessageToClient(string message)
+        {
+            NetworkStream networkStream = _clientSocket.GetStream();
+            var sendBytes = Encoding.ASCII.GetBytes(message);
+
+            networkStream.Write(sendBytes, 0, sendBytes.Length);
+            networkStream.Flush();
+
+            Console.WriteLine("[MessageFromServer]: " + message);
+        }
+
+
+
+        /// <summary>
+        /// Получение запросов клиента
+        /// </summary>
+        private void ClientReqest()
+        {
+            byte[] inputBytes = new byte[255];
+
+            while (true)
+            {
+                try
+                {
+                    //Получение сообщения от клиента
+                    NetworkStream networkStream = _clientSocket.GetStream();
+                    networkStream.Read(inputBytes, 0, 255);
+
+                    //Перевод сообщения из массива байтов в строку 
+                    var messageFromClient = Encoding.ASCII.GetString(inputBytes);
+                    messageFromClient = messageFromClient.Substring(0, messageFromClient.IndexOf("$"));
+
+                    //Вывод сообщения в лог сервера
+                    Console.WriteLine($"[MessageFromClient {_clientId}]: {messageFromClient}");
+
+                    //Обрабатываем сообщение
+                    MessageProcessing(messageFromClient);
+
+                }
+                catch 
+                {
+                    Console.WriteLine($"Client {_clientId} disconnect!");
+                    break;
+                }
+            }
+        }
+
+        private void MessageProcessing(string message)
+        {
+            switch (message)
+            {
+                case "GetVirtualMemory": SendingVirtualMemory(); break;
+                case "GetPhysicMemory": SendingPhysicMemory(); break;
+                case "GetNameServer" : ShowMessageToClient("Server2"); break;
+
+                default: break;
+            }
+        }
+
+        private void SendingVirtualMemory()
+        {
+            var result = _systemInfo.GetVirtualMemory();
+            ShowMessageToClient(result);
+        }
+
+        private void SendingPhysicMemory()
+        {
+            var result = _systemInfo.GetPhysicsMemory();
+            ShowMessageToClient(result);
+        }
+    }
+}
---
+++ Server2/Server2/SystemInfo.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using Topshelf.Runtime.Windows;
+
+namespace Server2
+{
+   
+    public class SystemInfo
+    {
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+        private class MEMORYSTATUSEX
+        {
+            public uint dwLength;           //Размер структуры
+            public uint dwMemoryLoad;       // Число от 0 до 100, указывающее приблизительный процент используемой физической памяти
+            public ulong ullTotalPhys;      //общее кол-во физической(оперативной) памяти
+            public ulong ullAvailPhys;      //свободное кол-во физической(оперативной) памяти
+            public ulong ullTotalPageFile;  //предел памяти для системы или текущего процесса
+            public ulong ullAvailPageFile;  //Максимальный объем памяти,который текущий процесс может передать в байтах.
+            public ulong ullTotalVirtual;   //общее количество виртуальной памяти(файл подкачки)
+            public ulong ullAvailVirtual;   //свободное количество виртуальной памяти(файл подкачки)
+            public ulong ullAvailExtendedVirtual;
+
+            public MEMORYSTATUSEX()
+            {
+                this.dwLength = (uint)Marshal.SizeOf(typeof(MEMORYSTATUSEX));
+            }
+        }
+
+        [return: MarshalAs(UnmanagedType.Bool)]
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern bool GlobalMemoryStatusEx([In, Out] MEMORYSTATUSEX lpBuffer);
+
+
+        public string GetPhysicsMemory()
+        {
+            
+            MEMORYSTATUSEX mEMORYSTATUSEX = new MEMORYSTATUSEX();
+
+            if (GlobalMemoryStatusEx(mEMORYSTATUSEX))
+            {
+                return mEMORYSTATUSEX.dwMemoryLoad.ToString();
+            }
+            else
+            {
+                return "Error API";
+            }
+        }
+
+        public string GetVirtualMemory()
+        {
+            MEMORYSTATUSEX mEMORYSTATUSEX = new MEMORYSTATUSEX();
+
+            if (GlobalMemoryStatusEx(mEMORYSTATUSEX))
+            {
+                var procent = mEMORYSTATUSEX.ullAvailVirtual / (mEMORYSTATUSEX.ullTotalVirtual / 100);
+                return procent.ToString();
+            }
+            else
+            {
+                return "Error API";
+            }
+        }
+    }
+}
